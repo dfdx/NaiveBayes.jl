1bca8ee6c7aeb2d0cff29adfc101fd3fc9b99edd
Andrei Zhabinski <faithlessfriend@gmail.com>
2014-12-08 09:49:52 +0300

    fixed outer constructor

--- src/nbtypes.jl
+++ src/nbtypes.jl
@@ -1,18 +1,19 @@
 
 abstract NBModel{C}
 
-type MultinomialNB{C} <: NBModel
+type MultinomialNB{C} # <: NBModel
     c_counts::Dict{C, Int64}           # count of ocurrences of each class
     x_counts::Dict{C, Vector{Number}}  # count/sum of occurrences of each var
     x_totals::Vector{Number}           # total occurrences of each var
-    n_obs::Int64                       # total number of seen observations
-    function MultinomialNB(classes::Vector{C}, n_vars::Int64; smoothing=1)
-        c_counts = Dict(classes, zeros(Int64, length(classes)))        
-        x_counts = Dict{C, Vector{Int64}}()
-        for c in classes
-            x_counts[c] = ones(Int64, n_vars) * smoothing
-        end
-        x_totals = ones(Float64, n_vars) * smoothing * length(c_counts)
-        new(c_counts, x_counts, x_totals, 0)
+    n_obs::Int64                       # total number of seen observations    
+end
+
+function MultinomialNB{C}(classes::Vector{C}, n_vars::Int64; smoothing=1)
+    c_counts = Dict(classes, zeros(Int64, length(classes)))        
+    x_counts = Dict{C, Vector{Int64}}()
+    for c in classes
+        x_counts[c] = ones(Int64, n_vars) * smoothing
     end
+    x_totals = ones(Float64, n_vars) * smoothing * length(c_counts)
+    MultinomialNB{C}(c_counts, x_counts, x_totals, 0)
 end
