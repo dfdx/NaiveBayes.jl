319c945e3c7d384568a915af7a873d78e3d8ad96
Andrei Zhabinski <faithlessfriend@gmail.com>
2016-07-20 09:47:50 +0300

    removed obsolete/forgotten code

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,16 +9,3 @@ else
 end
 
 include("core.jl")
-
-
-m = MultinomialNB([:a, :b, :c], 5)
-X1 = [1 2 5 2;
-      5 3 -2 1;
-      0 2 1 11;
-      6 -1 3 3;
-      5 7 7 1]
-
-y1 = [:a, :b, :a, :c]
-fit(m, X1, y1)
-      
-@test predict(m, X1) == y1
